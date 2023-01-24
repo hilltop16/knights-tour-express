fcfd68899812d26d567cf613c73dede22a8921ef
unknown <ben.lu@pixelmatic.com>
2023-01-23 20:46:58 -0600

    drew chess board

---
+++ public/javascripts/kt-api.js
@@ -0,0 +1,50 @@
+// http://knights-t.herokuapp.com/
+
+/*
+Welcome to Knights Tour.
+
+
+Endpoints: 
+/
+
+This documentation
+
+
+/legalMove
+
+Post Endpoint
+
+Request:
+
+row and col are integers between 0 and 7 inclusive.
+{ from: [col, row], to: [col, row] }
+
+Response
+
+{ from: [col, row], to: [col, row], isLegal: true/false }
+
+
+
+Sample:
+Request:
+{ "from": [0, 0], "to": [3, 5] }
+
+Response:
+{ "status": "ok", "from": [ 0, 0 ], "to": [ 3, 5 ], "isLegal": false }
+*/
+export async function legalMove(from, to) {
+  const response = await axios.post(
+    'http://knights-t.herokuapp.com/legalMove',
+    {
+      from,
+      to
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  )
+  
+  return response.data.isLegal
+}
---
+++ public/javascripts/main.js
@@ -0,0 +1,28 @@
+import { legalMove } from './kt-api.js'
+
+const grids = document.querySelectorAll('td');
+
+export const onContentLoaded = async () => {
+  const initialMove = [0, 0]
+
+  const result1 = await legalMove(initialMove, [1,2])
+  const result2 = await legalMove(initialMove, [2,4])
+  // console.log(`From: [${initialMove}] to [1, 2], legal: ${result1}`)
+  // console.log(`From: [${initialMove}] to [2, 4], legal: ${result2}`)
+
+  let currPos = initialMove;
+
+  grids.forEach(grid => {
+      grid.addEventListener('click', async (event) => {
+        console.log('event', event)
+        const moveY = event.target.className.split(' ')[0];
+        const moveX = event.target.parentElement.className;
+        const isValid = await legalMove(currPos, [moveX, moveY]);
+        console.log('move valid? ', isValid);
+        if (isValid) currPos = [moveX, moveY];
+      })
+  });
+}
+
+
+document.addEventListener('DOMContentLoaded', onContentLoaded)
